080a2a9e193118bb5e356fe24daa3cba4b072dac
Michail Karavaev <michail.karavaev@taxfix.de>
2022-08-10 23:43:03 +0200

    Add attack modes processing

--- lib/thrower/processor.ex
+++ lib/thrower/processor.ex
@@ -1,10 +1,33 @@
 defmodule Thrower.Processor do
   alias Thrower.RadarEntry
 
-  def run do
+  def run(attack_modes, indexed_radar_entries) do
+    do_run(attack_modes, indexed_radar_entries)
   end
+
+  defp do_run(attack_modes, indexed_radar_entries, prev_mode \\ nil, acc \\ [])
+  defp do_run([], _indexed_radar_entries, _prev_mode, acc), do: acc
+
+  defp do_run([attack_mode | tail], indexed_radar_entries, prev_mode, acc) do
+    if mode_applicable?(attack_mode, prev_mode) do
+      with {:ok, result} <- impl(attack_mode).apply(indexed_radar_entries) do
+        do_run(tail, indexed_radar_entries, attack_mode, [result|acc])
+      end
+    else
+      do_run(tail, indexed_radar_entries, prev_mode, acc)
+    end
+  end
+
 
   def parse_radar_entries(radar_entries) do
     Enum.map(radar_entries, &RadarEntry.new/1)
+  end
+
+  def mode_applicable?(current_mode, prev_mode) do
+    ! impl(current_mode).opposite_to?(prev_mode)
+  end
+
+  def impl(mode_name) do
+    Application.get_env(Thrower, :attack_modes)[mode_name]
   end
 end
--- test/thrower/processor_test.exs
+++ test/thrower/processor_test.exs
@@ -6,38 +6,6 @@ defmodule Thrower.ProcessorTest do
 
   describe "parse_radar_entries/1" do
     test "should wrap params into RadarEntries structs" do
-      test_params = [
-        %{
-          "position" => %{
-            "x" => 0,
-            "y" => 40
-          },
-          "villains" => [
-            %{
-              "costume" => "Shir Khan",
-              "malice" => 80
-            },
-            %{
-              "costume" => "Voldemort"
-            },
-            %{
-              "costume" => "Donald Duck"
-            }
-          ]
-        },
-        %{
-          "position" => %{
-            "x" => 0,
-            "y" => 20
-          },
-          "villains" => [
-            %{
-              "costume" => "Donald Duck"
-            }
-          ]
-        }
-      ]
-
       assert [
                %RadarEntry{
                  position: %RadarEntry.Position{x: 0, y: 40},
@@ -51,7 +19,7 @@ defmodule Thrower.ProcessorTest do
                  position: %RadarEntry.Position{x: 0, y: 20},
                  villains: [%RadarEntry.Villain{costume: "Donald Duck", malice: 0}]
                }
-             ] = Processor.parse_radar_entries(test_params)
+             ] = Processor.parse_radar_entries(test_params())
     end
 
     test "should raise an error in case bad params" do
@@ -73,5 +41,46 @@ defmodule Thrower.ProcessorTest do
         Processor.parse_radar_entries(bad_params)
       end)
     end
+  end
+
+  describe "run/2" do
+    test "should process all attack_modes" do
+      assert %{position: %{x: 0, y: 40}, villains: ["Shir Khan", "Voldemort"]} =
+               Processor.run([:closest_first], test_params())
+    end
+  end
+
+  defp test_params do
+    [
+      %{
+        "position" => %{
+          "x" => 0,
+          "y" => 40
+        },
+        "villains" => [
+          %{
+            "costume" => "Shir Khan",
+            "malice" => 80
+          },
+          %{
+            "costume" => "Voldemort"
+          },
+          %{
+            "costume" => "Donald Duck"
+          }
+        ]
+      },
+      %{
+        "position" => %{
+          "x" => 0,
+          "y" => 20
+        },
+        "villains" => [
+          %{
+            "costume" => "Donald Duck"
+          }
+        ]
+      }
+    ]
   end
 end
