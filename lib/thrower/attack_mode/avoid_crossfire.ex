434063eeda93cbaf297ca58403c30937149f83a8
Michail Karavaev <michail.karavaev@taxfix.de>
2022-08-11 23:37:40 +0200

    Add avoid_crossfire strategy

---
+++ lib/thrower/attack_mode/avoid_crossfire.ex
@@ -0,0 +1,32 @@
+defmodule Thrower.AttackMode.AvoidCrossfire do
+  @behaviour Thrower.AttackMode
+  alias Thrower.AttackMode
+
+  @good_characters ["Donald Duck"]
+
+  @impl AttackMode
+  def apply(radar_entries) do
+    radar_entries
+    |> filter_good_characters_area()
+    |> filter_with_empty_villains()
+    |> hd()
+  end
+
+  def opposite_to?(mode) do
+    false
+    # mode in @opposite_attack_modes
+  end
+
+  defp filter_good_characters_area(radar_entries) do
+    Enum.reject(radar_entries, fn entry ->
+      area_costumes = Enum.map(entry.villains, &(&1.costume))
+      ! MapSet.disjoint?(MapSet.new(area_costumes), MapSet.new(@good_characters))
+    end)
+  end
+
+  defp filter_with_empty_villains(radar_entries) do
+    Enum.reject(radar_entries, fn entry ->
+      Enum.empty?(entry.villains)
+    end)
+  end
+end
