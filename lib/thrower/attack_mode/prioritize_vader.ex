6bd73a02233626a6f88fe720682a6d798b8e5744
Michail Karavaev <michail.karavaev@taxfix.de>
2022-08-11 23:59:38 +0200

    Add prioritize_vader strategy

---
+++ lib/thrower/attack_mode/prioritize_vader.ex
@@ -0,0 +1,30 @@
+defmodule Thrower.AttackMode.PrioritizeVader do
+  @behaviour Thrower.AttackMode
+  alias Thrower.AttackMode
+
+  @good_characters ["Donald Duck"]
+
+  @impl AttackMode
+  def apply(radar_entries) do
+    radar_entries
+    |> get_with_vader()
+    |> exclude_good_characters()
+  end
+
+  def opposite_to?(mode) do
+    false
+    # mode in @opposite_attack_modes
+  end
+
+  defp exclude_good_characters(radar_entry) do
+    updated_villains_list = Enum.reject(radar_entry.villains, &(&1.costume in @good_characters))
+    %{radar_entry | villains: updated_villains_list}
+  end
+
+  defp get_with_vader(radar_entries) do
+    Enum.find(radar_entries, fn entry ->
+      area_costumes = Enum.map(entry.villains, &(&1.costume))
+      "Darth Vader" in area_costumes
+    end)
+  end
+end
---
+++ test/thrower/attack_mode/prioritize_vader_test.exs
@@ -0,0 +1,45 @@
+defmodule Thrower.AttackMode.PrioritizeVaderTest do
+  use ExUnit.Case
+  alias Thrower.AttackMode
+  alias Thrower.RadarEntry
+
+  test "should return only entry w/o good characters" do
+    entries = [
+      %RadarEntry{
+        position: %RadarEntry.Position{x: 0, y: 40},
+        villains: [
+          %RadarEntry.Villain{costume: "Donald Duck", malice: 0},
+          %RadarEntry.Villain{costume: "Darth Vader", malice: 20}
+        ]
+      },
+      %RadarEntry{
+        position: %RadarEntry.Position{x: 0, y: 20},
+        villains: [%RadarEntry.Villain{costume: "Voldemort", malice: 20}]
+      },
+      %RadarEntry{
+        position: %RadarEntry.Position{x: 0, y: 30},
+        villains: [%RadarEntry.Villain{costume: "Darth Vader", malice: 20}]
+      }
+    ]
+
+    assert %{
+      position: %{x: 0, y: 40},
+      villains: [%{costume: "Darth Vader"}]
+    } = AttackMode.PrioritizeVader.apply(entries)
+  end
+
+  test "should prioritize Darth Vader" do
+    entries = [
+      %RadarEntry{
+        position: %RadarEntry.Position{x: 0, y: 20},
+        villains: [%RadarEntry.Villain{costume: "Voldemort", malice: 20}]
+      },
+      %RadarEntry{
+        position: %RadarEntry.Position{x: 0, y: 30},
+        villains: [%RadarEntry.Villain{costume: "Darth Vader", malice: 20}]
+      }
+    ]
+
+    assert %{position: %{x: 0, y: 30}} = AttackMode.PrioritizeVader.apply(entries)
+  end
+end
